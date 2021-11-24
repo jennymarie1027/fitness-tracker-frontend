cdbe68123a94c697d5dd3726677a07ea279bf95c
Maureen <mlincke15@gmail.com>
2021-11-24 10:46:14 -0700

    building new routines form and wrapping myroutines route in a conditional statment to check if user is logged in

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -1,12 +1,77 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { handleLogin, handleRegister} from '../handleFuncs';
+import React, { useState, useEffect } from 'react';
 const { API_URL } = '../constants.js';
 
-const MyRoutines = () => {
+const MyRoutines = ({isLoggedIn}) => {
+    const [routineName, setRoutineName] = useState('')
+    const [routineGoal, setRoutineGoal] = useState('')
+    const [isPublic, setIsPublic] = useState(false)
+    const [myRoutines, setMyRoutines] = useState([])
+
     return (
         <div>
-            
+            <h1>My routines</h1>
+            <form
+                onSubmit={async (e) => {
+                    e.preventDefault();
+
+                    try {
+                        const response = await fetch(
+                            API_URL + '/api/routines',
+                        {
+                            method: "POST",
+                            body: JSON.stringify({
+                                routineName,
+                                routineGoal,
+                                isPublic
+                            })
+                        })
+                        
+                        const result = await response.json();
+                        setMyRoutines([
+                            ...myRoutines,
+                            result.data.name
+                        ])
+                    } catch (error) {
+                        console.error(error)
+                    }
+                }}
+            >
+                {/* <div><h2>Add New Routine</h2></div> */}
+                <div>
+                   <input 
+                        type='text'
+                        value={routineName}
+                        onChange={e => setRoutineName(e.target.value)}
+                        id='routineName'
+                        required
+                        placeholder='Routine Name'
+                   />
+                </div>
+                <div>
+                    <input 
+                        type='text'
+                        value={routineGoal}
+                        onChange={e => setRoutineGoal(e.target.value)}
+                        id='routineGoal'
+                        required
+                        placeholder='Routine Goal'
+                    />
+                </div>
+                <div>
+                    <input 
+                        type="checkbox"
+                        value={isPublic}
+                        onChange={e => setIsPublic(e.target.value)}
+                        id="isPublic"
+                        required
+                    >
+                        {/* <p>Public</p> */}
+                    </input>
+                </div>
+                <button type='submit' value='Submit'>
+                    <label>Submit New Routine</label>
+                </button>
+            </form>
         </div>
     )
 }
