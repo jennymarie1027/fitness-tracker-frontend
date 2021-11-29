cbb76ef04b9c02ff44b239e624415f4227f59ea2
Maureen <mlincke15@gmail.com>
2021-11-28 19:30:34 -0700

    merging master changes

--- src/components/User/newRoutine.js
+++ src/components/User/newRoutine.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { API_URL } from '../../constants';
+import API_URL from '../../constants';
 import { handleFetchingUserRoutines } from '../../handleFuncs';
 
 const NewRoutine = ({setMyRoutines, token, myRoutines}) => {
