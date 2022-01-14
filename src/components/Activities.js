8f80bb76b7f7f8ce5ad6764c6cf92a461b0cd09f
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-14 12:40:01 -0700

    activities rendered through a form, made the headers sticky so you can reference as you scroll

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Activities = ({ token, activities, history }) => {
     return (
-        <main style={{margin: 4.5 +'em'}}>
+        <main style={{margin: 4.5 +'em'}} className='allActivitiesContainer'>
             
         { token ? 
-            <button  className='btn btn-primary mb-3 mt-3'
+            <button  className='btn btn-primary mb-3 mt-3 newActivity'
                 onClick={() => {
                     history.push('/newactivity');
                     console.log(history)
@@ -15,19 +15,18 @@ const Activities = ({ token, activities, history }) => {
         <h1>Activities</h1> 
         <table>
             <thead>
-                <tr className='sticky-top'>
-                    <th>Activity Name</th>
-                    <th>Activity Description</th>
+                <tr>
+                    <th>Activity Name:</th>
+                    <th>Activity Description:</th>
                 </tr>
             </thead>
             <tbody>
                {activities.length ? (
                    activities.map(activity => (
-                       <>
-                        <tr scope='row' key={activity.id}></tr>
+                    <tr scope='row' key={activity.id} className='activityRow'>
                         <td>{activity.name}</td>
                         <td>{activity.description}</td>
-                       </>
+                    </tr>
                    ))
                ) : null} 
             </tbody>
