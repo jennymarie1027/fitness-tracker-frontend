76512753c033ec1c633921d3cceac4f5f92cd811
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-14 10:43:50 -0700

    deleted useEffect that was duplicated in the root index so unnecessary

--- src/components/CreateActivity.js
+++ src/components/CreateActivity.js
@@ -1,61 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import { handleFetchingActivities, handleAddingActivity } from '../handleFuncs.js';
 
-const CreateActivity = ({ setActivities, token, activities }) => {
+const CreateActivity = ({ setActivities, token }) => {
 
     const [name, setName] = useState('')
     const [description, setDescription] = useState('')
 
-    useEffect(() => {
-        setActivities(activities)
-    }, [description])
 
     return (
         <div>
             <h2>Create A New Activity:</h2>
-        <form onSubmit={async (e) => {
-            e.preventDefault();
-            try {
-                const data = await handleAddingActivity(name, description, token);
-                if (data.error) {
-                    alert('That activity already exists')
-                    setName('')
-                    setDescription('')
-                } else {
-                    const res = await handleFetchingActivities();
-                    setActivities(res);
-                    setName('')
-                    setDescription('')
+            <form onSubmit={async (e) => {
+                e.preventDefault();
+                try {
+                    const data = await handleAddingActivity(name, description, token);
+                    if (data.error) {
+                        alert('That activity already exists')
+                        setName('')
+                        setDescription('')
+                    } else {
+                        const res = await handleFetchingActivities();
+                        setActivities(res);
+                        setName('')
+                        setDescription('')
+                    }
+                } catch (err) {
+                    console.error(err)
                 }
-            } catch (err) {
-                console.error(err)
-            }
-        
-        }}>
-        <div>
-            <label>Name:</label>
-            <input 
-                id='name'
-                type='text'
-                placeholder='enter activity name'
-                required
-                value={name}
-                onChange={e => setName(e.target.value)}
-            />
-        </div>
-        <div>
-            <label>Description:</label>
-            <input 
-                id='description'
-                type='text'
-                placeholder='enter activity description'
-                required
-                value={description}
-                onChange={e => setDescription(e.target.value)}
-            />
-        </div>
-        <button>Submit!</button>
-        </form>
+            
+            }}>
+            <div>
+                <label>Name:</label>
+                <input 
+                    id='name'
+                    type='text'
+                    placeholder='enter activity name'
+                    required
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label>Description:</label>
+                <input 
+                    id='description'
+                    type='text'
+                    placeholder='enter activity description'
+                    required
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
+            </div>
+            <button>Submit!</button>
+            </form>
         </div>
     )
 }
