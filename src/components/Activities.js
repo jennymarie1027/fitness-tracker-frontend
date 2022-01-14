3cf55b3984d9c08d2d92b3d2da94bef08c683b8c
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-14 11:52:06 -0700

    added a btn and connected that to new CreateActivity route

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import CreateActivity from './CreateActivity';
-const API_URL = '../constants.js';
-const { handleFetchingActivities } = '../handleFuncs.js'
 
-const Activities = ({ token, activities, setActivities }) => {
+const Activities = ({ token, activities, setActivities, history }) => {
     return (
         <main style={{margin: 4 +'em'}}>
-        { token ? <CreateActivity setActivities={setActivities} token={token} /> : null }
+            <button onClick={() => {
+                history.push('/newactivity');
+                console.log(history)
+            }}>Create a New Activity</button>
+        {/* { token ? <CreateActivity setActivities={setActivities} token={token} /> : null } */}
 
         <h1>All Activities</h1> 
         {activities &&
